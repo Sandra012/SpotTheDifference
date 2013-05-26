06106dbb3596bf8898c4b5d4aa04ae6eecf8b619
andrejgasteovski <andrej.gasteovski@gmail.com>
2013-05-26 13:00:12 +0200

    New version

--- SpotTheDifference/Form1.cs
+++ SpotTheDifference/Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -13,7 +13,6 @@ namespace SpotTheDifference
 {
     public partial class Form1 : Form
     {
-
         public Category category;
         Form2 form2;
 
@@ -27,52 +26,68 @@ namespace SpotTheDifference
             e.Graphics.Clear(Color.Black);
         }
 
-        private void btnCancel_Click(object sender, EventArgs e)
-        {
-            DialogResult result = MessageBox.Show("Are you sure you want to exit?", "Exit", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
-            if (result == DialogResult.Yes) Application.Exit();
-        }
-
-        private void pbAnimals_MouseEnter(object sender, EventArgs e)
-        {
-           pbAnimals.BackgroundImage = Properties.Resources.std10_v2;
-        }
-
         private void pbAnimals_Click(object sender, EventArgs e)
         {
-            category = new Category("animals");
-            form2 = new Form2(this);
-            form2.Show();
-          
+            CreateNewCategory("animals");
         }
 
         private void pbFinki_Click(object sender, EventArgs e)
         {
-            category = new Category("finki");
-            form2 = new Form2(this);
-            form2.Show();
+            CreateNewCategory("finki");
         }
 
         private void pbNature_Click(object sender, EventArgs e)
-        {   
-            category = new Category("nature");
-            form2 = new Form2(this);
-            form2.Show();
+        {
+            CreateNewCategory("nature");
         }
 
         private void pbSkopje_Click(object sender, EventArgs e)
+        {
+            CreateNewCategory("skopje");      
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Are you sure you want to exit?", "Exit", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes) 
+                Application.Exit();
+        }
+
+        private void btnInstr_Click(object sender, EventArgs e)
         {            
-            category = new Category("skopje");
+            MessageBox.Show(String.Format("\t\t\tInstructions:\n  Point and click on the differences between two pictures as fast as you can.\n\tRemember! Each wrong click will reduce the time left."));
+        }
+
+        private void pbAnimals_MouseEnter(object sender, EventArgs e)
+        {
+            lbSelect.Text = "Animals";
+        }
+
+        private void pbFinki_MouseEnter(object sender, EventArgs e)
+        {
+            lbSelect.Text = "FINKI";
+        }
+
+        private void pbNature_MouseEnter(object sender, EventArgs e)
+        {
+            lbSelect.Text = "Nature";
+        }
+
+        private void pbSkopje_MouseEnter(object sender, EventArgs e)
+        {
+            lbSelect.Text = "Skopje";
+        }
+
+        private void Form1_MouseEnter(object sender, EventArgs e)
+        {
+            lbSelect.Text = "";
+        }
+
+        private void CreateNewCategory(String categoryName)
+        {
+            category = new Category(categoryName);
             form2 = new Form2(this);
             form2.Show();
-            
         }
-        
-        private void btnInstr_Click(object sender, EventArgs e)
-        {
-            
-            MessageBox.Show("Instructions: /n Point and click on the differences between two pictures as fast as you can. /n Remember! Each wrong click will reduce the time left.");
-        }
-        //This is an old version of this file..
     }
 }
